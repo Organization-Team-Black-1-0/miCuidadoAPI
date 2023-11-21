887e01a2db101205de82ccf1679edc60e48e2d64
Asiul Rosillo <zeazeta@gmail.com>
2023-11-20 19:18:29 -0600

    se quito el .js que estaba de mas

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 
 //app.use(authenticateToken);
 
-app.use("/users", userRoutes.js);
+app.use("/users", userRoutes);
 
 app.use((req, res, next) => {
     res.status(404).json({ error: 'Ruta no encontrada' });
