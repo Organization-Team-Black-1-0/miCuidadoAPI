a54b279287c6032b79f806d51164821c8a717dc6
Cisho <cischass@gmail.com>
2023-11-23 00:45:50 -0300

    Nuevos cambios

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 import 'dotenv/config'
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import email from '../reset-password/email/email.js';
+import nodemailer from 'nodemailer';
+import { google } from 'googleapis';
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded ({ extended: false }))
 app.set('view engine', 'ejs')
 
+// Se definen las credenciales del dominio desde el cual se va a enviar el correo electrónico.
+// Estas credenciales deben estar en un documento .env.
+
+const CLIENT_ID = '571089471111-jldn6ivl6ue7hadgk6s1pdbg2qkfvt0p.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = '1//042vA_rtRDRk3CgYIARAAGAQSNwF-L9Ir0ZN6QD_GMy3DA48gJxtTOjuBZ-ZygnPtwySx_vULq_NBI5pkWtwb7MwqotmpvKhf9MQ';
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
+
+// Aquí se define un objeto con un usuario. Nótese que aquí debería estar conectada la base de datos.
 let user = {
     id: "yellow",
-    email: "somebody@gmail.com",
+    email: "cischass@gmail.com",
     password: "adsdasdfasdfasdfasdf"
 }
 
+// El mensaje que define la constante JWT_SECRET puede ser cualquier cadena de texto, ej.: "tucolorfavorito".
+// Esta constante reemplaza el uso de la librería bcrypt, aunque es más recomendable usar esta última. Por esto mismo,
+// la cadena de texto debe ir en un archivo .env. 
+
 const JWT_SECRET = 'some super secret...' //(se debe agregar un archivo .env para resguardar las claves)
 
+// Se definen las rutas
 
 app.get('/', (req, res) => {
     res.send('Hello world');
@@ -37,6 +55,8 @@ app.post('/forgot-password', (req, res, next) =>{
     //Si el usuario existe, 
     //se le envía un link de 15 minutos de duración
     //para reestablecer contraseña
+    // El objeto payload permite diseñar los campos necesarios para definir el email y el id
+    // de usuario
 
     const secret = JWT_SECRET + user.password
     const payload = {
@@ -45,9 +65,57 @@ app.post('/forgot-password', (req, res, next) =>{
     }
     const token = jwt.sign(payload, secret, {expiresIn: '15m'}); //expiresIn es un método de la librería JWT.
     const link = `http://localhost:3000/reset-password/${user.id}/${token}`;
-    console.log(link)
-    res.send("La contraseña ha sido reestablecida. El link ha sido enviado a su correo");
-});
+    const correo = "https://mail.google.com/mail/u/1/#inbox"
+    res.send(`La contraseña ha sido reestablecida. El link ha sido enviado a su <a href=${correo}>correo</a>`);
+  
+    // función asíncrona en la que se agregan los datos de acceso y credenciales del
+    // dominio desde el cual se envía el correo. En este caso, lo hice desde google mientras se crea 
+    // un dominio. 
+async function sendMail() {
+    try {
+      const accessToken = await oAuth2Client.getAccessToken()
+      
+      const transport = nodemailer.createTransport({
+        service: 'gmail',
+        secure: false,
+        port: 25,
+        auth: {
+            type: 'OAuth2',
+            user: 'checerovitch@gmail.com',
+            clientId: '571089471111-jldn6ivl6ue7hadgk6s1pdbg2qkfvt0p.apps.googleusercontent.com',
+            clientSecret: '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',
+            refreshToken: 'https://developers.google.com/oauthplayground',
+            accessToken: 'ya29.a0AfB_byDU_0gy3OMtXeGoNPdZqm6mIyXwb95q68XtoCjGCZ87QOUGapcDXurPkk55zRKP-tH7Cdho4USjuqctNSCQXu8aU83bE14EXspUqkIM-nxTi2TZRX1xnA9H9cnTVPhw7K2wfxXUPVmlJTKXjA_Wd7TYjsqIX9RVaCgYKAVcSARMSFQHGX2MilRI277OwaOwYy2Cb_IzxWA0171'
+        },
+          tls: {
+            rejectUnauthorized: false
+      }
+      })
+
+    // En mailOptions se diseña el esquema del correo electrónico.
+    // Nótese que en el campo del destinatario (to) se agrega el método
+    // user.mail para que este recupere de la base de datos el correo electrónico 
+    // que haya sido tipeado por el usuario.
+
+      const mailOptions = {
+        from: 'Checerovitch <checerovitch@gmail.com>',
+        to: user.email,
+        subject: "Hello from gmail using api",
+        text: "Hello from gmail using api",
+        html: `Click the following link to reset your password: <a href=${link}>Here!<a/>`,
+      };
+
+      const result = await transport.sendMail(mailOptions)
+      return result
+    }   catch (error) {
+        return error
+    }
+}
+// se llama la función asíncrona sendMail() junto a un .catch, al que se 
+// le pasa un mensaje de error.
+sendMail().then(result=> console.log('Correo enviado', result))
+.catch(error => console.log(error.message));
+   });
 
 app.get('/reset-password/:id/:token', (req, res, next) =>{
     const {id, token} = req.params;
@@ -72,10 +140,10 @@ app.post('/reset-password/:id/:token', (req, res, next) =>{
     const {id, token} = req.params;
     const {password, password2} = req.body;
    
-    //verificar si el usuario existe en la base de datos
-     if(id!==user.id){
-        res.send("Usuario inválido");
-        return;
+    // Se verifica si ambas claves coinciden
+    if(password !== password2){
+        res.send("Las contraseñas no coinciden");
+        return
     }
     
     const secret = JWT_SECRET + user.password;
@@ -85,7 +153,7 @@ app.post('/reset-password/:id/:token', (req, res, next) =>{
         //Para encontrar al usuario con payload, se usa el email y el id del usuario y luego se actualiza la clave
         //Siempre hacer un hash de la contraseña antes de enviarla
         user.password = password;
-        res.send(user);
+        res.send("La contraseña ha sido reestablecida");
 
     } catch (error) {
         console.log(error.message);
